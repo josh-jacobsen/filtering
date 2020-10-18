491bcc8ee463623b53b036020bba22cdb6628bf6
Josh Jacobsen <josh.jacobsen@myfoodbag.co.nz>
2020-10-18 16:53:29 +1300

    Fetch and display data

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,64 +1,93 @@
-import React, { Component, Fragment } from "react";
+import React, { Fragment } from "react";
 import Hero from "./Hero";
 import authConfig from "../auth_config.json";
 import { Auth0ContextInterface, withAuth0 } from '@auth0/auth0-react';
-import { Alert } from "reactstrap";
+import { Alert, Container, Row, Col } from "reactstrap";
+
+async function GetFlightData<T>(request: RequestInfo, auth0: Auth0ContextInterface): Promise<T> {
+  const getAccessTokenSilently = await auth0.getAccessTokenSilently();
+
+  const response = await fetch(request, {
+    method: 'GET',
+    headers: {
+      'Authorization': `Bearer ${getAccessTokenSilently}`
+    }
+  });
+
+  const body = await response.json() as Promise<T>;
+
+  return body;
+}
+
+var createUniqieId = function () {
+  return '_' + Math.random().toString(36).substr(2, 9);
+};
 
 interface HomeProps {
   auth0: Auth0ContextInterface;
 }
 
+interface ColumnData {
+  id: string,
+  colType: String,
+  numRows: Number,
+  numUniqueValues: Number,
+  sample: String[],
+  sampleHeader: String
+}
+
+interface AggregateData {
+  columns: ColumnData[],
+  numColumns: Number,
+  numRows: Number,
+}
+
+interface FlightData {
+  data: AggregateData
+  status: String
+}
+
 interface HomeState {
-  currentUser: User;
-  error: string;
-  loading: boolean;
+  flightData: FlightData[],
+  error: string,
+  loading: boolean
 }
 
-interface User {
-  id: string;
-  email: string;
-}
+class Home extends React.Component<HomeProps, HomeState>
+{
+  constructor(props: HomeProps) {
+    super(props);
+  }
 
-class Home extends Component<HomeProps> {
-  state = {
-    currentUser: undefined,
+  public readonly state: Readonly<HomeState> = {
+    flightData: new Array<FlightData>(),
     error: '',
-    loading: false
-  };
+    loading: true
+  }
 
-  componentDidMount () {
+  componentDidMount() {
     const { isAuthenticated } = this.props.auth0;
     if (isAuthenticated) {
       this.getCurrentUserData();
     }
   }
 
-  async getCurrentUserData () {
+  async getCurrentUserData() {
     this.setState({ loading: true, error: '' });
-
-    const url = `${authConfig.apiBase}/current-user`;
-    const getAccessTokenSilently = await this.props.auth0.getAccessTokenSilently();
-
-    const response = await fetch(url, {
-      method: 'GET',
-      headers: {
-        'Authorization': `Bearer ${getAccessTokenSilently}`
-      }
-    });
-
-    if (!response.ok) {
-      const error = `An error has occured: ${response.status}`;
-      this.setState({ error });
-      return;
-    }
-  
-    const { data } = await response.json();
-
-    this.setState({ currentUser: data, loading: false });
+    const url = `${authConfig.apiBase}/synopsis`;
+    const data2: FlightData[] = await GetFlightData<FlightData[]>(url, this.props.auth0);
+    this.setState({ flightData: data2, loading: false })    
   }
 
-  render () {
-    const { currentUser, error, loading } = this.state;
+  render() {
+    const { error, loading } = this.state;
+    const aaaaa = this.state.flightData as unknown as FlightData;
+    if (!loading) {
+      aaaaa.data.columns.forEach(element => {
+        element.id = createUniqieId()
+      });
+    }
+
     return (
       <Fragment>
         <Hero />
@@ -67,13 +96,37 @@ class Home extends Component<HomeProps> {
         }
         {error &&
           <Alert color="danger">
-            {error}  
+            {error}
           </Alert>
         }
-        {currentUser &&
-          <div className="text-center">
-            {JSON.stringify(currentUser)}
-          </div>
+        {(!loading && !error) &&
+          <Container>
+          Filters
+          </Container>
+        }
+        {(!loading && !error) &&
+          <Container>
+            
+          {aaaaa.data.columns.map(a =>
+          <Row key={a.id}>
+            <Col>
+                Column Type:{a.colType}
+            </Col>
+            <Col>
+              Number of Rows: {a.numRows}
+            </Col>
+            <Col>
+              Number of unique values: {a.numUniqueValues}
+            </Col>
+            <Col>
+              Sample Header: {a.sampleHeader}
+            </Col>
+            <Col>
+              Sample: {a.sample}
+              </Col>
+            </Row>
+          )}
+          </Container>
         }
       </Fragment>
     );
